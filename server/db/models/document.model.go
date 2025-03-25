61856c24deda711181ea64f993b045c494c6d55f
Om Gupta <omgupta0720@gmail.com>
2025-03-25 23:49:33 +0530

    add: document model

---
+++ server/db/models/document.model.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// Document represents a collection of blocks
+type Document struct {
+	ID        string    `gorm:"primaryKey;type:uuid;default:gen_random_uuid()" json:"id"`
+
+	// Document metadata
+	Title     string    `json:"title"`
+	RootBlockID *string  `gorm:"type:uuid;index" json:"rootBlockId"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	
+	// Virtual field - not stored in database
+	Blocks []Block `gorm:"-" json:"blocks,omitempty"`
+}
